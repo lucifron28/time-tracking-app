cbf51532d346c44c84bb094971cdbee156c59ed8
Ron <lucifron28@gmail.com>
2025-05-21 00:29:43 +0800

    added all entries

---
+++ lib/screens/all_entries.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class AllEntries extends StatelessWidget {
+  const AllEntries({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center();
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,46 +11,74 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          physics: BouncingScrollPhysics(),
-          children: [
-            SizedBox(
-              height: 100,
-              child: DrawerHeader(
-                decoration: BoxDecoration(color: Colors.lightBlueAccent),
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Text(
-                    "Menu",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            physics: BouncingScrollPhysics(),
+            children: [
+              SizedBox(
+                height: 100,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(color: Colors.lightBlueAccent),
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Menu",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            ListTile(
-              leading: Icon(Icons.folder),
-              title: Text("Projects"),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ProjectManagement()),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.list_alt_outlined),
-              title: Text("Tasks"),
-            ),
+              ListTile(
+                leading: Icon(Icons.folder),
+                title: Text("Projects"),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ProjectManagement(),
+                    ),
+                  );
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.list_alt_outlined),
+                title: Text("Tasks"),
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text("Time Tracking"),
+          backgroundColor: Colors.lightBlueAccent,
+          centerTitle: true,
+          bottom: TabBar(
+            tabs: [
+              Tab(child: Text("All Entries")),
+              Tab(child: Text("Grouped By Project")),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(child: Text("All Entries")),
+            Center(child: Text("Grouped by Projects")),
           ],
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+
+          },
+          backgroundColor: Colors.amberAccent,
+          child: Icon(Icons.add),
+        ),
       ),
-      appBar: AppBar(title: Text("Time Tracking"), centerTitle: true),
     );
   }
 }
