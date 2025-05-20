242370ebd72beee2a0f59c5d1222451da4a7282c
Ron <lucifron28@gmail.com>
2025-05-20 23:35:09 +0800

    drawer added

--- lib/screens/home_sceen.dart
+++ lib/screens/home_sceen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';;
+import 'package:flutter/material.dart';
 
 
 class HomeScreen extends StatefulWidget {
@@ -12,7 +12,38 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      drawer: Drawer(
+        child: ListView(
+          physics: BouncingScrollPhysics(),
+          children: [
+            SizedBox(
+              height: 100,
+              child: DrawerHeader(
+                decoration: BoxDecoration(color: Colors.grey),
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Menu",
+                    style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+            ),
+            ListTile(
+              leading: Icon(Icons.folder),
+              title: Text("Projects"),
+            ),
+            ListTile(
+              leading: Icon(Icons.list_alt_outlined),
+              title: Text("Tasks"),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text("Time Tracking"),
+        centerTitle: true,
+      ),
     );
   }
 }
