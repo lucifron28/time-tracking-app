72b5c7b4332070c3095101e6de205b3950ff635e
Ron <lucifron28@gmail.com>
2025-05-20 23:34:54 +0800

    updated class name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:time_tracking/main.dart';
+import 'package:time_tracking/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const TimeTrackingApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
