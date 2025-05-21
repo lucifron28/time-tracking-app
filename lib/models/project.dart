4e8613569611ae8e7111a4c4fa16dfc1cfa4d084
Ron <lucifron28@gmail.com>
2025-05-21 10:19:21 +0800

    full project model

--- lib/models/project.dart
+++ lib/models/project.dart
@@ -1,11 +1,23 @@
-import 'package:flutter/material.dart';
-
 class Project {
-  final int id;
+  final String id;
   final String name;
 
   Project({
     required this.id,
     required this.name,
   });
+
+  factory Project.fromJson(Map<String, dynamic> json) {
+    return Project(
+      id: json['id'] as String,
+      name: json['name'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+    };
+  }
 }
