7a8635a14b9f61f678b1ec4a9921e4ab03fc28ae
Ron Vincent Cada <lucifron28@gmail.com>
2025-05-21 14:21:40 +0800

    added hive

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'package:time_tracking/providers/time_entry_provider.dart';
 import 'package:time_tracking/providers/project_management_provider.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:time_tracking/app.dart';
+import 'package:time_tracking/models/project.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
+  Hive.registerAdapter(ProjectAdapter());
   await Hive.openBox('time_entries');
   await Hive.openBox('projects');
   runApp(
