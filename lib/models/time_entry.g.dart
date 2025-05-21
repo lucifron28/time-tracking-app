e601319d007c7b495bf7370c3be76d107a0970b6
Ron Vincent Cada <lucifron28@gmail.com>
2025-05-21 15:26:45 +0800

    generated model

---
+++ lib/models/time_entry.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'time_entry.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TimeEntryAdapter extends TypeAdapter<TimeEntry> {
+  @override
+  final int typeId = 0;
+
+  @override
+  TimeEntry read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TimeEntry(
+      id: fields[0] as String?,
+      projectId: fields[1] as String,
+      taskId: fields[2] as String,
+      totalMinutes: fields[3] as int,
+      date: fields[4] as DateTime,
+      notes: fields[5] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, TimeEntry obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.projectId)
+      ..writeByte(2)
+      ..write(obj.taskId)
+      ..writeByte(3)
+      ..write(obj.totalMinutes)
+      ..writeByte(4)
+      ..write(obj.date)
+      ..writeByte(5)
+      ..write(obj.notes);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TimeEntryAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
