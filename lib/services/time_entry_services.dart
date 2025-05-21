53b3de494d77aaf360a3bcc41fea8bebc4476847
Ron Vincent Cada <lucifron28@gmail.com>
2025-05-21 15:23:29 +0800

    create service

---
+++ lib/services/time_entry_services.dart
@@ -0,0 +1,34 @@
+import 'package:hive/hive.dart';
+import 'package:time_tracking/models/time_entry.dart';
+
+class TimeEntryServices {
+  static const String _boxName = 'time_entries';
+
+  static Future<void> init() async {
+    if (!Hive.isBoxOpen(_boxName)) {
+      await Hive.openBox<TimeEntry>(_boxName);
+    }
+  }
+
+  static Box<TimeEntry> get _box => Hive.box<TimeEntry>(_boxName);
+
+  static Future<void> addTimeEntry(TimeEntry timeEntry) async {
+    await _box.put(timeEntry.id, timeEntry);
+  }
+
+  static List<TimeEntry> getTimeEntries() {
+    return _box.values.toList();
+  }
+
+  static Future<void> updateTimeEntry(TimeEntry timeEntry) async {
+    await _box.put(timeEntry.id, timeEntry);
+  }
+
+  static Future<void> deleteTimeEntry(String id) async {
+    await _box.delete(id);
+  }
+
+  static Future<void> deleteAllTimeEntries() async {
+    await _box.clear();
+  }
+}
