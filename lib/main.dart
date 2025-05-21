de93949a452c2e976e7f87e0cb9bc0ede2550ad7
Ron Vincent Cada <lucifron28@gmail.com>
2025-05-21 15:22:39 +0800

    add taskAdapter

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,22 @@ import 'package:time_tracking/providers/project_management_provider.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:time_tracking/app.dart';
 import 'package:time_tracking/models/project.dart';
+import 'package:time_tracking/models/task.dart';
+import 'package:time_tracking/models/time_entry.dart';
 import 'package:time_tracking/services/project_services.dart';
 import 'package:time_tracking/services/task_services.dart';
 import 'package:time_tracking/providers/task_management_provider.dart';
+import 'package:time_tracking/services/time_entry_services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   Hive.registerAdapter(ProjectAdapter());
+  Hive.registerAdapter(TaskAdapter());
   await Hive.openBox('time_entries');
   await ProjectServices.init();
   await TaskServices.init();
+  await TimeEntryServices.init();
   runApp(
     MultiProvider(
       providers: [
