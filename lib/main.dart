02abf827b5f400659190128f83d5eac6a237490a
Ron <lucifron28@gmail.com>
2025-05-21 10:58:15 +0800

    initialized hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/time_entry_provider.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('time_entries');
   runApp(
     ChangeNotifierProvider(
       create: (context) => TimeEntryProvider(),
