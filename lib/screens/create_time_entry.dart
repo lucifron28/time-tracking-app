aff574fa42b2e5a3449be4b5a570d1884937f2af
Ron <lucifron28@gmail.com>
2025-05-21 10:01:50 +0800

    Time Entry Screen added

---
+++ lib/screens/create_time_entry.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+
+class CreateTimeEntry extends StatelessWidget {
+  const CreateTimeEntry({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Create an entry"),
+        centerTitle: true,
+      ),
+      body: CreateTimeEntryScreen(),
+    );
+  }
+}
+
+
+class CreateTimeEntryScreen extends StatefulWidget {
+  const CreateTimeEntryScreen({super.key});
+
+  @override
+  State<CreateTimeEntryScreen> createState() => _CreateTimeEntryScreenState();
+}
+
+class _CreateTimeEntryScreenState extends State<CreateTimeEntryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Create Time Entry Screen"),
+    );
+  }
+}
