ef91c23bf66a3e882183f6d3b92cbae0cd071061
Ron <lucifron28@gmail.com>
2025-05-21 10:59:11 +0800

    created time entry provider

---
+++ lib/providers/time_entry_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import '../models/time_entry.dart';
+
+class TimeEntryProvider with ChangeNotifier {
+  final List<TimeEntry> _entries = [];
+
+  List<TimeEntry> get entries => _entries;
+
+  void addEntry(TimeEntry entry) {
+    _entries.add(entry);
+    notifyListeners();
+  }
+
+  void deleteEntry(String id) {
+    _entries.removeWhere((entry) => entry.id == id);
+    notifyListeners();
+  }
+
+  
+}
