e8ab0a0d8f16be91c2980c98b25ae5506b48856d
Ron Vincent Cada <lucifron28@gmail.com>
2025-05-21 17:02:23 +0800

    create tab bar view for group by projects

---
+++ lib/screens/tab_bar_views/group_by_projects.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracking/providers/time_entry_provider.dart';
+import 'package:time_tracking/models/time_entry.dart';
+import 'package:provider/provider.dart';
+
+class GroupByProjects extends StatefulWidget {
+  const GroupByProjects({super.key});
+
+  @override
+  State<GroupByProjects> createState() => _GroupByProjectsState();
+}
+
+class _GroupByProjectsState extends State<GroupByProjects> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await Provider.of<TimeEntryProvider>(context, listen: false).loadEntries();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TimeEntryProvider>(
+      builder: (context, provider, child) {
+        final entries = provider.entries;
+        if (entries.isEmpty) {
+          return Center(child: Text("No Entries"));
+        }
+
+        final groupedEntries = <String, List<TimeEntry>>{};
+
+        for (var entry in entries) {
+          groupedEntries.putIfAbsent(entry.projectName, () => []).add(entry);
+        }
+
+        return ListView(
+          children: groupedEntries.entries.map((entry) {
+            return ExpansionTile(
+              title: Text(entry.key),
+              children: entry.value.map((e) {
+                return ListTile(
+                  title: Text(e.taskName),
+                  subtitle: Text("${e.totalMinutes} min"),
+                );
+              }).toList(),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}
