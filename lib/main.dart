7f5be450c25e83e72ff05cdb6cc81c4eca1c43b1
Ron Vincent Cada <lucifron28@gmail.com>
2025-05-21 13:35:21 +0800

    added project management provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'providers/time_entry_provider.dart';
+import 'package:time_tracking/providers/time_entry_provider.dart';
+import 'package:time_tracking/providers/project_management_provider.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'app.dart';
+import 'package:time_tracking/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox('time_entries');
+  await Hive.openBox('projects');
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => TimeEntryProvider(),
-      child: TimeTrackingApp()
-      )
-    );
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => TimeEntryProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ProjectManagementProvider(),
+        ),
+      ],
+      child: TimeTrackingApp(),
+    ),
+  );
 }
