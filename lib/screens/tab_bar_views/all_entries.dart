c7de335837f2e20c0b95743fc94a89b01e5b1be4
Ron Vincent Cada <lucifron28@gmail.com>
2025-05-21 16:55:57 +0800

    created all entries tab bar view

---
+++ lib/screens/tab_bar_views/all_entries.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracking/providers/time_entry_provider.dart';
+import 'package:provider/provider.dart';
+
+
+class AllEntries extends StatefulWidget {
+  const AllEntries({super.key});
+
+  @override
+  State<AllEntries> createState() => _AllEntriesState();
+}
+
+class _AllEntriesState extends State<AllEntries> {
+
+  @override
+  void initState(){
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await Provider.of<TimeEntryProvider>(context, listen: false).loadEntries();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Consumer<TimeEntryProvider>(
+        builder: (context, provider, child) {
+          final entries = provider.entries;
+          if (entries.isEmpty) {
+            return Text("No Entries");
+          }
+          return ListView.builder(
+            itemCount: entries.length,
+            itemBuilder: (context, index) {
+              final entry = entries[index];
+              return ListTile(
+                style: ListTileStyle.drawer,
+                leading: Icon(Icons.access_time),
+                title: Text(entry.projectName),
+                subtitle: Text(entry.taskName),
+                trailing: Text("${entry.totalMinutes} min"),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
